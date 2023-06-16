db426df4086473e785956cd9af401c0e80851f57
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-06-16 08:28:25 +0200

    added important files

---
+++ assets/generator/news.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests, random
+
+#url = "https://realpython.github.io/fake-jobs/"
+#page = requests.get(url)
+
+#soup = BeautifulSoup(page.content, "html.parser")
+
+print(random.randrange(0, 2))
